e2734bd1ef8050e907dbf8441504130f2387e783
TheRealGioviok <64363733+TheRealGioviok@users.noreply.github.com>
2021-12-10 21:31:09 +0100

    Uci command parsing end execution

--- ChessGrizzly/ChessGrizzly.cpp
+++ ChessGrizzly/ChessGrizzly.cpp
@@ -2,32 +2,29 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "BBmacros.h"
 #include "Move.h"
 #include "tables.h"
 #include "Game.h"
 #include "Position.h"
+#include "uci.h"
 #include <intrin.h>
 int main() {
     std::cout << "Chess Grizzly by Gioviok\n";
     initAll();
 
     Game game;
-    game.parseFEN("8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1");
 
-    std::cout << encodeMove(d5, c6, P, p, EMPTY, true, false, false, false) << " = ";
-    printMove(encodeMove(d5, c6, P, p, EMPTY, true, false, false, false));
-    std::cout << "\n";
 
-    getchar();
-    //game.makeMove(encodeMove(d5, c6, P, p, EMPTY, true, false, false, false));
+    // We test the uci command parser
     game.print();
-    for (int i = 1; i <= 7; i++) {
-        std::cout << game.divide(i) << "\n";
-        std::cout << "==================================" << std::endl;
-        getchar();
+    while (1) {
+        std::string command;
+        std::getline(std::cin, command);
+        executeCommand(&game, (char*)command.c_str());
+        game.print();
     }
-    
-
     return 0;
 }
