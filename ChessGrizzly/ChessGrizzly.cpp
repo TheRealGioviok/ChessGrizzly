7a6e7252b9123a762a889cf701fac93cff00e7f7
TheRealGioviok <64363733+TheRealGioviok@users.noreply.github.com>
2021-12-10 03:19:49 +0100

    Move generation completed, 100% on 25 perft test suite

--- ChessGrizzly/ChessGrizzly.cpp
+++ ChessGrizzly/ChessGrizzly.cpp
@@ -5,20 +5,29 @@
 #include "BBmacros.h"
 #include "Move.h"
 #include "tables.h"
+#include "Game.h"
 #include "Position.h"
 #include <intrin.h>
-int main()
-{
+int main() {
     std::cout << "Chess Grizzly by Gioviok\n";
     initAll();
 
-    Position pos;
+    Game game;
+    game.parseFEN("8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1");
+
+    std::cout << encodeMove(d5, c6, P, p, EMPTY, true, false, false, false) << " = ";
+    printMove(encodeMove(d5, c6, P, p, EMPTY, true, false, false, false));
+    std::cout << "\n";
+
+    getchar();
+    //game.makeMove(encodeMove(d5, c6, P, p, EMPTY, true, false, false, false));
+    game.print();
+    for (int i = 1; i <= 7; i++) {
+        std::cout << game.divide(i) << "\n";
+        std::cout << "==================================" << std::endl;
+        getchar();
+    }
     
-    pos.printBoard();
 
-    MoveList moveList;
-
-    pos.generateMoves(&moveList);
-
-    //printMoveList(&moveList);
+    return 0;
 }
