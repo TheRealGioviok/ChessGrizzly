cea0b60a5d33d5d4bc7568a6823ecc273a4ab690
TheRealGioviok <64363733+TheRealGioviok@users.noreply.github.com>
2021-12-20 19:01:33 +0100

    Mobility evaluation, pawn evaluation, rook evaluation

--- ChessGrizzly/evaluation.cpp
+++ ChessGrizzly/evaluation.cpp
@@ -1,4 +1,5 @@
 #include "evaluation.h"
+#include "tables.h"
 
 Score simpleEval(Position *pos){
     // We will return the sum of the material of the board, from the player to move perspective.
@@ -57,15 +58,65 @@ void initTables(){
     }
     std::cout << std::endl;
 }
+#define MG_PAWNMOB 3
+#define MG_PAWN_PASSER_BONUS 5
+#define EG_PAWNMOB 5
+#define EG_PAWN_PASSER_BONUS 15
+#define MG_DOUBLEDPAWN -15
+#define MG_ISOLATEDPAWN -5
+#define MG_BACKWARDPAWN -5
+#define EG_DOUBLEDPAWN -40
+#define EG_ISOLATEDPAWN -15
+#define EG_BACKWARDPAWN -15
+
+#define MG_KNIGHTMOB 4
+#define EG_KNIGHTMOB 5
+#define MG_ATTACKEDKNIGHTMOB 2
+#define EG_ATTACKEDKNIGHTMOB 2
+
+#define MG_BISHOPMOB 2
+#define EG_BISHOPMOB 3
+#define MG_ATTACKEDBISHOPMOB 1
+#define EG_ATTACKEDBISHOPMOB 2
+
+#define MG_ROOKMOB 1
+#define EG_ROOKMOB 3
+#define MG_ATTACKEDROOKMOB 0
+#define EG_ATTACKEDROOKMOB 1
+#define MG_ROOKSEMIOPENFILE 5
+#define MG_ROOKOPENFILE 10
+#define EG_ROOKSEMIOPENFILE 10
+#define EG_ROOKOPENFILE 20
+#define MG_ROOKSEVENTH 15
+#define EG_ROOKSEVENTH 20
+
+#define MG_QUEENMOB 1
+#define EG_QUEENMOB 5
+#define MG_ATTACKEDQUEENMOB 0
+#define EG_ATTACKEDQUEENMOB 2
+
+
+#define MOBILITYEVAL true
+
+
 
 Score pestoEval(Position *pos){
     Score mg[2] = { 0, 0 };
     Score eg[2] = { 0, 0 };
     Score gamePhase = 0;
 
+    BitBoard whiteOccupancy = pos->bitboards[P] | pos->bitboards[N] | pos->bitboards[B] | pos->bitboards[R] | pos->bitboards[Q] | pos->bitboards[K];
+    BitBoard blackOccupancy = pos->bitboards[p] | pos->bitboards[n] | pos->bitboards[b] | pos->bitboards[r] | pos->bitboards[q] | pos->bitboards[k];
+    BitBoard occupancy = whiteOccupancy | blackOccupancy;
+    BitBoard freeSquares = ~occupancy;
+
+    BitBoard attackedByWhitePawns = 0ULL;
+    BitBoard attackedByBlackPawns = 0ULL;
+
     // White pawns P, N, B, R, Q, K - p, n, b, r, q, k
     {
-        BitBoard pawns = pos->bitboards[P];
+        BitBoard pawns = pos->bitboards[P]; // We need this copy because we will modify it
+        BitBoard pawnsBB = pawns; // We need this copy to evaluate positional scores of pawns
         while (pawns){
             unsigned long pawn;
             bitScanForward(&pawn, pawns);
@@ -74,6 +125,29 @@ Score pestoEval(Position *pos){
             mg[WHITE] += mgTables[P][pawn];
             eg[WHITE] += egTables[P][pawn];
             gamePhase = gamephaseInc[P];
+#if MOBILITYEVAL
+            // we also will account for the mobility of the pawn
+            // we will add the number of squares that the pawn can move to
+            // we will add the number of squares that the pawn can attack
+            // 1: can the pawn move forward?
+            mg[WHITE] += ((squaresAhead[WHITE][pawn] & freeSquares) > 0) * MG_PAWNMOB;
+            eg[WHITE] += ((squaresAhead[WHITE][pawn] & freeSquares) > 0) * EG_PAWNMOB;
+            // 2: is the pawn a passed pawn?
+            mg[WHITE] += ((squaresAhead[WHITE][pawn] & blackOccupancy) == 0) * MG_PAWN_PASSER_BONUS;
+            eg[WHITE] += ((squaresAhead[WHITE][pawn] & blackOccupancy) == 0) * EG_PAWN_PASSER_BONUS;
+            // 3: is the pawn a doubled pawn?
+            mg[WHITE] += ((pawnsBB & squaresAhead[WHITE][pawn]) > 0) * MG_DOUBLEDPAWN;
+            eg[WHITE] += ((pawnsBB & squaresAhead[WHITE][pawn]) > 0) * EG_DOUBLEDPAWN;
+            // 4: is the pawn an isolated pawn?
+            mg[WHITE] += ((pawnsBB & adjacentFiles[fileOf(pawn)]) == 0) * MG_ISOLATEDPAWN;
+            eg[WHITE] += ((pawnsBB & adjacentFiles[fileOf(pawn)]) == 0) * EG_ISOLATEDPAWN;
+            // 5: is the pawn a backward pawn?
+            mg[WHITE] += ((pawnsBB & backwardPawnMask[WHITE][pawn]) == 0) * MG_BACKWARDPAWN;
+            eg[WHITE] += ((pawnsBB & backwardPawnMask[WHITE][pawn]) == 0) * EG_BACKWARDPAWN;
+
+            // we update the attackedByWhitePawns bitboard
+            attackedByWhitePawns |= pawnAttacks[WHITE][pawn];
+#endif
         }
     }
     // Black pawns
@@ -87,6 +161,30 @@ Score pestoEval(Position *pos){
             mg[BLACK] += mgTables[p][pawn];
             eg[BLACK] += egTables[p][pawn];
             gamePhase = gamephaseInc[p];
+
+#if MOBILITYEVAL
+            // we also will account for the mobility of the pawn
+            // we will add the number of squares that the pawn can move to
+            // we will add the number of squares that the pawn can attack
+            // 1: can the pawn move forward?
+            mg[BLACK] += ((squaresAhead[BLACK][pawn] & freeSquares) > 0) * MG_PAWNMOB;
+            eg[BLACK] += ((squaresAhead[BLACK][pawn] & freeSquares) > 0) * EG_PAWNMOB;
+            // 2: is the pawn a passed pawn?
+            mg[BLACK] += ((squaresAhead[BLACK][pawn] & whiteOccupancy) == 0) * MG_PAWN_PASSER_BONUS;
+            eg[BLACK] += ((squaresAhead[BLACK][pawn] & whiteOccupancy) == 0) * EG_PAWN_PASSER_BONUS;
+            // 3: is the pawn a doubled pawn?
+            mg[BLACK] += ((pawns & squaresAhead[BLACK][pawn]) > 0) * MG_DOUBLEDPAWN;
+            eg[BLACK] += ((pawns & squaresAhead[BLACK][pawn]) > 0) * EG_DOUBLEDPAWN;
+            // 4: is the pawn an isolated pawn?
+            mg[BLACK] += ((pawns & adjacentFiles[fileOf(pawn)]) == 0) * MG_ISOLATEDPAWN;
+            eg[BLACK] += ((pawns & adjacentFiles[fileOf(pawn)]) == 0) * EG_ISOLATEDPAWN;
+            // 5: is the pawn a backward pawn?
+            mg[BLACK] += ((pawns & backwardPawnMask[BLACK][pawn]) == 0) * MG_BACKWARDPAWN;
+            eg[BLACK] += ((pawns & backwardPawnMask[BLACK][pawn]) == 0) * EG_BACKWARDPAWN;
+            
+            // we update the attackedByBlackPawns bitboard
+            attackedByBlackPawns |= pawnAttacks[BLACK][pawn];
+#endif
         }
     }
     // White knights
@@ -100,6 +198,17 @@ Score pestoEval(Position *pos){
             mg[WHITE] += mgTables[N][knight];
             eg[WHITE] += egTables[N][knight];
             gamePhase = gamephaseInc[N];
+
+#if MOBILITYEVAL
+            // we also will account for the mobility of the knight
+            // we will add the number of squares that the knight can move to (and give them a score based on if they are attacked by an enemy pawn)
+            BitBoard moves = knightAttacks[knight];
+
+            mg[WHITE] += popCount((moves & attackedByBlackPawns)) * MG_ATTACKEDKNIGHTMOB;
+            mg[WHITE] += popCount((moves & ~attackedByBlackPawns)) * MG_KNIGHTMOB;
+            eg[WHITE] += popCount((moves & attackedByBlackPawns)) * EG_ATTACKEDKNIGHTMOB;
+            eg[WHITE] += popCount((moves & ~attackedByBlackPawns)) * EG_KNIGHTMOB;
+#endif
         }
     }
     // Black knights
@@ -113,6 +222,17 @@ Score pestoEval(Position *pos){
             mg[BLACK] += mgTables[n][knight];
             eg[BLACK] += egTables[n][knight];
             gamePhase = gamephaseInc[n];
+
+#if MOBILITYEVAL
+            // we also will account for the mobility of the knight
+            // we will add the number of squares that the knight can move to (and give them a score based on if they are attacked by an enemy pawn)
+            BitBoard moves = knightAttacks[knight];
+
+            mg[BLACK] += popCount((moves & attackedByWhitePawns)) * MG_ATTACKEDKNIGHTMOB;
+            mg[BLACK] += popCount((moves & ~attackedByWhitePawns)) * MG_KNIGHTMOB;  
+            eg[BLACK] += popCount((moves & attackedByWhitePawns)) * EG_ATTACKEDKNIGHTMOB;
+            eg[BLACK] += popCount((moves & ~attackedByWhitePawns)) * EG_KNIGHTMOB;
+#endif
         }
     }
     // White bishops
@@ -126,6 +246,17 @@ Score pestoEval(Position *pos){
             mg[WHITE] += mgTables[B][bishop];
             eg[WHITE] += egTables[B][bishop];
             gamePhase = gamephaseInc[B];
+
+#if MOBILITYEVAL
+            // we also will account for the mobility of the bishop
+            // we will add the number of squares that the bishop can move to (and give them a score based on if they are attacked by an enemy pawn)
+            BitBoard moves = getBishopAttack(bishop, occupancy) & ~whiteOccupancy;
+
+            mg[WHITE] += popCount((moves & attackedByBlackPawns)) * MG_ATTACKEDBISHOPMOB;
+            mg[WHITE] += popCount((moves & ~attackedByBlackPawns)) * MG_BISHOPMOB;
+            eg[WHITE] += popCount((moves & attackedByBlackPawns)) * EG_ATTACKEDBISHOPMOB;
+            eg[WHITE] += popCount((moves & ~attackedByBlackPawns)) * EG_BISHOPMOB;
+#endif
         }
     }
     // Black bishops
@@ -139,6 +270,17 @@ Score pestoEval(Position *pos){
             mg[BLACK] += mgTables[b][bishop];
             eg[BLACK] += egTables[b][bishop];
             gamePhase = gamephaseInc[b];
+
+#if MOBILITYEVAL
+            // we also will account for the mobility of the bishop
+            // we will add the number of squares that the bishop can move to (and give them a score based on if they are attacked by an enemy pawn)
+            BitBoard moves = getBishopAttack(bishop, occupancy) & ~blackOccupancy;
+
+            mg[BLACK] += popCount((moves & attackedByWhitePawns)) * MG_ATTACKEDBISHOPMOB;
+            mg[BLACK] += popCount((moves & ~attackedByWhitePawns)) * MG_BISHOPMOB;
+            eg[BLACK] += popCount((moves & attackedByWhitePawns)) * EG_ATTACKEDBISHOPMOB;
+            eg[BLACK] += popCount((moves & ~attackedByWhitePawns)) * EG_BISHOPMOB;
+#endif
         }
     }
     // White rooks
@@ -152,6 +294,25 @@ Score pestoEval(Position *pos){
             mg[WHITE] += mgTables[R][rook];
             eg[WHITE] += egTables[R][rook];
             gamePhase = gamephaseInc[R];
+
+#if MOBILITYEVAL
+            // we also will account for the mobility of the rook
+            // we will add the number of squares that the rook can move to (and give them a score based on if they are attacked by an enemy pawn)
+            BitBoard moves = getRookAttack(rook, occupancy) & ~whiteOccupancy;
+
+            // mg[WHITE] += popCount((moves & attackedByBlackPawns)) * MG_ATTACKEDROOKMOB; (Since MG_ATTACKEDROOKMOB is 0)
+            mg[WHITE] += popCount((moves & ~attackedByBlackPawns)) * MG_ROOKMOB;
+            eg[WHITE] += popCount((moves & attackedByBlackPawns)) * EG_ATTACKEDROOKMOB;
+            eg[WHITE] += popCount((moves & ~attackedByBlackPawns)) * EG_ROOKMOB;
+
+            // We will give a bonus to the rook if it is on a semi-open file
+            mg[WHITE] += ((squaresAhead[WHITE][rook] & blackOccupancy) == 0) * MG_ROOKSEMIOPENFILE;
+            eg[WHITE] += ((squaresAhead[WHITE][rook] & blackOccupancy) == 0) * EG_ROOKSEMIOPENFILE;
+            mg[WHITE] += ((squaresAhead[WHITE][rook] & occupancy) == 0) * MG_ROOKOPENFILE;
+            eg[WHITE] += ((squaresAhead[WHITE][rook] & occupancy) == 0) * EG_ROOKOPENFILE;
+            mg[WHITE] += (rankOf(rook) <= 1) * MG_ROOKSEVENTH;
+            eg[WHITE] += (rankOf(rook) <= 1) * EG_ROOKSEVENTH;
+#endif
         }
     }
     // Black rooks
@@ -165,6 +326,25 @@ Score pestoEval(Position *pos){
             mg[BLACK] += mgTables[r][rook];
             eg[BLACK] += egTables[r][rook];
             gamePhase = gamephaseInc[r];
+
+#if MOBILITYEVAL
+            // we also will account for the mobility of the rook
+            // we will add the number of squares that the rook can move to (and give them a score based on if they are attacked by an enemy pawn)   
+            BitBoard moves = getRookAttack(rook, occupancy) & ~blackOccupancy;
+
+            // mg[BLACK] += popCount((moves & attackedByWhitePawns)) * MG_ATTACKEDROOKMOB; (Since MG_ATTACKEDROOKMOB is 0)
+            mg[BLACK] += popCount((moves & ~attackedByWhitePawns)) * MG_ROOKMOB;
+            eg[BLACK] += popCount((moves & attackedByWhitePawns)) * EG_ATTACKEDROOKMOB;
+            eg[BLACK] += popCount((moves & ~attackedByWhitePawns)) * EG_ROOKMOB;
+
+            // We will give a bonus to the rook if it is on a semi-open file
+            mg[BLACK] += ((squaresAhead[BLACK][rook] & whiteOccupancy) == 0) * MG_ROOKSEMIOPENFILE;
+            eg[BLACK] += ((squaresAhead[BLACK][rook] & whiteOccupancy) == 0) * EG_ROOKSEMIOPENFILE;
+            mg[BLACK] += ((squaresAhead[BLACK][rook] & occupancy) == 0) * MG_ROOKOPENFILE;
+            eg[BLACK] += ((squaresAhead[BLACK][rook] & occupancy) == 0) * EG_ROOKOPENFILE;
+            mg[BLACK] += (rankOf(rook) >= 6) * MG_ROOKSEVENTH;
+            eg[BLACK] += (rankOf(rook) >= 6) * EG_ROOKSEVENTH;
+#endif
         }
     }
     // White queens
@@ -178,6 +358,17 @@ Score pestoEval(Position *pos){
             mg[WHITE] += mgTables[Q][queen];
             eg[WHITE] += egTables[Q][queen];
             gamePhase = gamephaseInc[Q];
+
+#if MOBILITYEVAL
+            // we also will account for the mobility of the queen
+            // we will add the number of squares that the queen can move to (and give them a score based on if they are attacked by an enemy pawn)
+            BitBoard moves = getQueenAttack(queen, occupancy) & ~whiteOccupancy;
+
+            // mg[WHITE] += popCount((moves & attackedByBlackPawns)) * MG_ATTACKEDQUEENMOB; (Since MG_ATTACKEDQUEENMOB is 0)
+            mg[WHITE] += popCount((moves & ~attackedByBlackPawns)) * MG_QUEENMOB;
+            eg[WHITE] += popCount((moves & attackedByBlackPawns)) * EG_ATTACKEDQUEENMOB;
+            eg[WHITE] += popCount((moves & ~attackedByBlackPawns)) * EG_QUEENMOB;
+#endif
         }
     }
     //Black queens
@@ -191,6 +382,17 @@ Score pestoEval(Position *pos){
             mg[BLACK] += mgTables[q][queen];
             eg[BLACK] += egTables[q][queen];
             gamePhase = gamephaseInc[q];
+
+#if MOBILITYEVAL
+            // we also will account for the mobility of the queen
+            // we will add the number of squares that the queen can move to (and give them a score based on if they are attacked by an enemy pawn)
+            BitBoard moves = getQueenAttack(queen, occupancy) & ~blackOccupancy;
+
+            // mg[BLACK] += popCount((moves & attackedByWhitePawns)) * MG_ATTACKEDQUEENMOB; (Since MG_ATTACKEDQUEENMOB is 0)
+            mg[BLACK] += popCount((moves & ~attackedByWhitePawns)) * MG_QUEENMOB;
+            eg[BLACK] += popCount((moves & attackedByWhitePawns)) * EG_ATTACKEDQUEENMOB;
+            eg[BLACK] += popCount((moves & ~attackedByWhitePawns)) * EG_QUEENMOB;
+#endif
         }
     }
     // White King (no need to iterate, since there is only one king per side)
